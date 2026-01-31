bc93c19054965e5d887bb6291372c6f859e47eb1
Ismoilov005 <Bekzodbekismoilov005@gmail.com>
2026-01-31 10:44:45 +0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-
+python manage.py shell -c "from quizzes.models import User; User.objects.create_superuser('algoarchitect', 'bekzodbekismoilov005@mail.com', 'adu-it-ki-1215') if not User.objects.filter(username='algoarchitect').exists() else None"
 python manage.py collectstatic --no-input
 python manage.py migrate
